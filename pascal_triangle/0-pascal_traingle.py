7ec27e0866210aa19a091c80a1aec0cd3e9495a8
idarapatrick <essienidara67@gmail.com>
2024-10-13 20:38:13 +0200

    edit

--- pascal_triangle/0-pascal_traingle.py
+++ pascal_triangle/0-pascal_traingle.py
@@ -8,12 +8,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Start with the first row as [1]
+    triangle = [[1]]  
 
     for i in range(1, n):
-        row = [1]  # Each row starts with 1
+        row = [1]  
         for j in range(1, i):
-            # Each element is the sum of two elements from the previous row
+           
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)  # Each row ends with 1
         triangle.append(row)
